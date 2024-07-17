6ecaeefa7cb52b64711fb7e5ec56821b490a358b
rampenke <rampenke@gmail.com>
2024-07-17 11:25:36 -0700

    logo added

--- lib/main.dart
+++ lib/main.dart
@@ -18,30 +18,39 @@ class DoodleDragon extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Please agree to the terms and conditions.'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              child: Text('Agree'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SketchScreen(geminiApiKey: dotenv.get('GEMINI_API_KEY', fallback: ''),
-                      openaiApiKey: dotenv.get('OPENAI_API_KEY', fallback: ''))),
-                );
-              },
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/doodle_dragon_logo.png'),
+            fit: BoxFit.contain,  // This will cover the entire background of the container
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Please agree to the terms and conditions.'),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Agree'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SketchScreen(
+                        geminiApiKey: dotenv.get('GEMINI_API_KEY', fallback: ''),
+                        openaiApiKey: dotenv.get('OPENAI_API_KEY', fallback: '')
+                    )),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
