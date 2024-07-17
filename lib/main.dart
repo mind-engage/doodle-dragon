b73564fa49d59870dca1e263136ec9df3b31867d
rampenke <rampenke@gmail.com>
2024-07-17 11:33:32 -0700

    home screen logo animation

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,74 @@ class DoodleDragon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sketch App',
+      title: 'Doodle Dragon',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple, // Choose a vibrant color scheme
+        fontFamily: 'ComicSansMS', // A kid-friendly font if available
+      ),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    )..repeat(reverse: true);
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome'),
-      ),
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/doodle_dragon_logo.png'),
-            fit: BoxFit.contain,  // This will cover the entire background of the container
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.pink, Colors.yellow, Colors.lightBlueAccent], // Use a playful gradient
           ),
         ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Please agree to the terms and conditions.'),
-              SizedBox(height: 20),
+              RotationTransition(
+                turns: _animation,
+                child: Image.asset(
+                  'assets/doodle_dragon_logo.png',
+                  height: 200, // Adjust size as needed
+                ),
+              ),
+              SizedBox(height: 40),
               ElevatedButton(
-                child: Text('Agree'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // Make the button visually appealing
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  textStyle: TextStyle(fontSize: 20),
+                ),
+                child: Text('Start Drawing!'),
                 onPressed: () {
                   Navigator.push(
                     context,
